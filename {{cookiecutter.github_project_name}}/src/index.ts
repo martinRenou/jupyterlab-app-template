a0cc54014c2eb8c596b63425f112c266e4e1b7e4
martinRenou <martin.renou@gmail.com>
2023-04-04 17:27:24 +0200

    Federated extensions

--- {{cookiecutter.github_project_name}}/src/index.ts
+++ {{cookiecutter.github_project_name}}/src/index.ts
@@ -11,16 +11,149 @@ import { App } from './app/app';
 
 import '../style/index.css';
 
+function loadScript(url: string) {
+  return new Promise((resolve, reject) => {
+    const newScript = document.createElement('script');
+    newScript.onerror = reject;
+    newScript.onload = resolve;
+    newScript.async = true;
+    document.head.appendChild(newScript);
+    newScript.src = url;
+  });
+}
+
+async function loadComponent(url: string, scope: string) {
+  await loadScript(url);
+
+  // From MIT-licensed https://github.com/module-federation/module-federation-examples/blob/af043acd6be1718ee195b2511adf6011fba4233c/advanced-api/dynamic-remotes/app1/src/App.js#L6-L12
+  // eslint-disable-next-line no-undef
+  // @ts-ignore
+  await __webpack_init_sharing__('default');
+  // @ts-ignore
+  const container = window._JUPYTERLAB[scope];
+  // Initialize the container, it may provide shared modules and may need ours
+  // eslint-disable-next-line no-undef
+  // @ts-ignore
+  await container.init(__webpack_share_scopes__.default);
+}
+
+async function createModule(scope: string, module: string) {
+  try {
+    // @ts-ignore
+    const factory = await window._JUPYTERLAB[scope].get(module);
+    return factory();
+  } catch (e) {
+    console.warn(
+      `Failed to create module: package: ${scope}; module: ${module}`
+    );
+    throw e;
+  }
+}
+
 /**
  * The main function
  */
 async function main(): Promise<void> {
   const app = new App();
+
+  // Add here the list of disabled extensions
+  const disabled: string[] = [];
+
+  /**
+   * Iterate over active plugins in an extension.
+   *
+   * #### Notes
+   * This also populates the disabled
+   */
+  function* activePlugins(extension: any) {
+    // Handle commonjs or es2015 modules
+    let exports;
+    if (Object.prototype.hasOwnProperty.call(extension, '__esModule')) {
+      exports = extension.default;
+    } else {
+      // CommonJS exports.
+      exports = extension;
+    }
+
+    let plugins = Array.isArray(exports) ? exports : [exports];
+    for (let plugin of plugins as any[]) {
+      if (PageConfig.Extension.isDisabled(plugin.id)) {
+        disabled.push(plugin.id);
+        continue;
+      }
+      yield plugin;
+    }
+  }
+
   const mods = [
     require('./plugins/paths'),
     require('./plugins/top'),
     require('./plugins/example')
   ];
+
+  const federatedExtensionPromises: Promise<any>[] = [];
+  const federatedStylePromises: Promise<any>[] = [];
+
+  const extension_data = JSON.parse(
+    PageConfig.getOption('federated_extensions')
+  );
+
+  const extensions: PromiseSettledResult<any>[] = await Promise.allSettled(
+    extension_data.map(async (data: any) => {
+      await loadComponent(
+        `${URLExt.join(
+          PageConfig.getOption('fullLabextensionsUrl'),
+          data.name,
+          data.load
+        )}`,
+        data.name
+      );
+      return data;
+    })
+  );
+
+  extensions.forEach((p: any) => {
+    if (p.status === 'rejected') {
+      // There was an error loading the component
+      console.error(p.reason);
+      return;
+    }
+
+    const data = p.value;
+    if (data.extension) {
+      federatedExtensionPromises.push(createModule(data.name, data.extension));
+    }
+    if (data.mimeExtension) {
+      // TODO Do we need mime extensions?
+      return;
+    }
+    if (data.style && !PageConfig.Extension.isDisabled(data.name)) {
+      federatedStylePromises.push(createModule(data.name, data.style));
+    }
+  });
+
+  // Add the federated extensions.
+  const federatedExtensions = await Promise.allSettled(
+    federatedExtensionPromises
+  );
+  federatedExtensions.forEach(p => {
+    if (p.status === 'fulfilled') {
+      for (let plugin of activePlugins(p.value)) {
+        if (!disabled.includes(plugin.id)) {
+          mods.push(plugin);
+        }
+      }
+    } else {
+      console.error(p.reason);
+    }
+  });
+
+  // Load all federated component styles and log errors for any that do not
+  (await Promise.allSettled(federatedStylePromises))
+    .filter(({ status }) => status === 'rejected')
+    .forEach(e => {
+      console.error((e as any).reason);
+    });
 
   app.registerPluginModules(mods);
 
--- {{cookiecutter.github_project_name}}/webpack.config.js
+++ {{cookiecutter.github_project_name}}/webpack.config.js
@@ -1,51 +1,73 @@
+const fs = require('fs-extra');
 const path = require('path');
 const webpack = require('webpack');
+const merge = require('webpack-merge').default;
+const { ModuleFederationPlugin } = webpack.container;
 
-module.exports = {
-  entry: ['whatwg-fetch', './lib/index.js'],
-  output: {
-    path: path.resolve(__dirname, '{{cookiecutter.python_package_name}}', 'static'),
-    filename: 'bundle.js'
-  },
-  bail: true,
-  devtool: 'source-map',
-  mode: 'development',
-  module: {
-    rules: [
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.html$/, use: 'file-loader' },
-      { test: /\.md$/, use: 'raw-loader' },
-      { test: /\.js.map$/, use: 'file-loader' },
-      {
-        // In .css files, svg is loaded as a data URI.
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        issuer: /\.css$/,
-        use: {
-          loader: 'svg-url-loader',
-          options: { encoding: 'none', limit: 10000 }
-        }
+const Build = require('@jupyterlab/builder').Build;
+const baseConfig = require('@jupyterlab/builder/lib/webpack.config.base');
+
+const data = require('./package.json');
+
+const names = Object.keys(data.dependencies).filter(name => {
+  try {
+    const packageData = require(path.join(name, 'package.json'));
+    return packageData.jupyterlab !== undefined;
+  } catch {
+    return false;
+  }
+});
+
+const distRoot = path.resolve(__dirname, '{{cookiecutter.python_package_name}}', 'static');
+
+// Ensure a clear build directory.
+const buildDir = path.resolve(__dirname, 'build');
+if (fs.existsSync(buildDir)) {
+  fs.removeSync(buildDir);
+}
+fs.ensureDirSync(buildDir);
+
+// Copy files to the build directory
+const libDir = path.resolve(__dirname, 'lib');
+fs.copySync(libDir, buildDir);
+
+const extras = Build.ensureAssets({
+  packageNames: names,
+  output: buildDir,
+  schemaOutput: path.resolve(__dirname, '{{cookiecutter.python_package_name}}'),
+  themeOutput: path.resolve(__dirname, '{{cookiecutter.python_package_name}}'),
+});
+
+// Make a bootstrap entrypoint
+const entryPoint = path.join(buildDir, 'bootstrap.js');
+
+if (process.env.NODE_ENV === 'production') {
+  baseConfig.mode = 'production';
+}
+
+module.exports = [
+  merge(baseConfig, {
+    mode: 'development',
+    entry: ['./' + path.relative(__dirname, entryPoint)],
+    output: {
+      path: distRoot,
+      library: {
+        type: 'var',
+        name: ['_JUPYTERLAB', 'CORE_OUTPUT']
       },
-      {
-        // In .ts and .tsx files (both of which compile to .js), svg files
-        // must be loaded as a raw string instead of data URIs.
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        issuer: /\.js$/,
-        use: {
-          loader: 'raw-loader'
+      filename: 'app.js'
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        library: {
+          type: 'var',
+          name: ['_JUPYTERLAB', 'CORE_LIBRARY_FEDERATION']
+        },
+        name: 'CORE_FEDERATION',
+        shared: {
+          ...data.dependencies
         }
-      },
-      {
-        test: /\.(png|jpg|gif|ttf|woff|woff2|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: [{ loader: 'url-loader', options: { limit: 10000 } }]
-      }
+      })
     ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      // Needed for Blueprint. See https://github.com/palantir/blueprint/issues/4393
-      'process.env': '{}',
-      // Needed for various packages using cwd(), like the path polyfill
-      process: { cwd: () => '/' }
-    })
-  ]
-};
+  })
+].concat(extras);
